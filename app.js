b24a20fa59d90c0fc35c270502dc5620a7f61aa4
Ganta Sowmya <160483851+sowmyaganta19@users.noreply.github.com>
2025-06-25 22:24:42 +0530

    level_1_task_3

--- app.js
+++ app.js
@@ -1,43 +1,98 @@
-let menu=document.querySelector("#menu-icon");
-let navbar=document.querySelector(".navbar");
-// adding togging effect for navbar
-menu.onclick=()=>{
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}
+let inp=document.querySelector('input');
+let btns=document.querySelectorAll('button');
 
-let sections = document.querySelectorAll('section');
-let navlink = document.querySelectorAll('header navbar a ');
-
-// adding scrolling to navbar automatically
-window.onscroll=()=>{
-    sections.forEach(sec=>{
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-
-        if(top>=offset && top<offset+height){
-            navlink.forEach(lin=>{
-                lin.classList.remove('active');
-                document.querySelector(`header navbar a[href*=${id}]`).classList.add('active');
-            })
+for(let i=0;i<btns.length;++i){
+    btns[i].addEventListener('click',()=>{
+        let val=btns[i].innerText;
+        if(val=='1'){
+            inp.value=`${inp.value}${val}`;
+        }
+        else if(val=='2'){
+            inp.value=`${inp.value}${val}`;
+        }
+        else if(val=='3'){
+            inp.value=`${inp.value}${val}`;
+        }
+        else if(val=='4'){
+            inp.value=`${inp.value}${val}`;
+        }
+        else if(val=='5'){
+            inp.value=`${inp.value}${val}`;
+        }
+        else if(val=='6'){
+            inp.value=`${inp.value}${val}`;
+        }
+        else if(val=='7'){
+            inp.value=`${inp.value}${val}`;
+        }
+        else if(val=='8'){
+            inp.value=`${inp.value}${val}`;
+        }
+        else if(val=='9'){
+            inp.value=`${inp.value}${val}`;
+        }
+        else if(val=='+'){
+            inp.value=`${inp.value}${val}`;
+        }
+        else if(val=='-'){
+            inp.value=`${inp.value}${val}`;
+        }
+        else if(val=='X'){
+            inp.value=`${inp.value}*`;
+        }
+        else if(val=='/'){
+            inp.value=`${inp.value}${val}`;
+        }
+        else if(val=='.'){
+            inp.value=`${inp.value}${val}`;
+        }
+        else if(val=='0'){
+            inp.value=`${inp.value}${val}`;
+        }
+        else if(val=='AC'){
+            inp.value="";
+        }
+        else if(val=="del"){
+            if(inp.value.length){
+                inp.value=inp.value.slice(0,inp.value.length-1);
+            }
+        }
+        else if(val=='+/-'){
+            console.log(inp[0]);
+            if(inp.value[0]!='-'){
+                inp.value=`-${inp.value}`;
+            }
+            else{
+                if(inp.value[0]=='-'){
+                    inp.value=inp.value.slice(1,inp.value.length);
+                }
+            }
+        }
+        else if(val=='%'){
+            inp.value=`${inp.value}${val}`;
+        }
+        else if(val=='='){
+            let equ=inp.value;
+            try{
+                inp.value=eval(equ);
+            } 
+            catch(err){
+                console.log(err);
+                inp.placeholder="enter valid input";
+                inp.value="";
+            }         
         }
     })
-    menu.classList.remove('bx-x');
-    navbar.classList.remove('active');
 }
 
-// scroll reveal for effectiveness
 ScrollReveal({
-    // reset : true,
+    reset : true,
     distance: '80px',
-    duration: 2000,
+    duration: 3000,
     delay: 200,
 });
 
-// adding of scrolling effects to page
-ScrollReveal().reveal('header',{origin:'top'})
-ScrollReveal().reveal('.social-media,.about-content,.skill-content,.project-box,.scroll-container,footer',{origin:'bottom'});
-ScrollReveal().reveal('.btn,.home-content h3,.about-img,.skills h1,.heading,.input-box',{origin:'left'})
-ScrollReveal().reveal('.home-content p,.about p,.home h1,.home-img,.certificate-box h3,textarea',{origin:'right'})
+ScrollReveal().reveal('.body',{origin:'top'})
+ScrollReveal().reveal('.values button',{origin:'bottom'});
+ScrollReveal().reveal('h1',{origin:'left'})
+ScrollReveal().reveal('.container',{origin:'right'})
